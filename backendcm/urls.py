150ef533efc0ff6cca91781f629032d8d2404d35
OmarYahya2 <omar.yahya965@gmail.com>
2025-09-07 15:38:24 +0300

    Tes

--- backendcm/urls.py
+++ backendcm/urls.py
@@ -1,8 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse  
+
+
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Backend is running on Render 🚀"
+    })
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('account.urls')),
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include("account.urls")),
 ]
